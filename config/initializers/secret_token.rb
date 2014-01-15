ac6645c585af41a082570b256c231c68371cf1a9
EC2 Default User <ec2-user@ip-172-31-26-198.us-west-1.compute.internal>
2014-01-15 23:10:15 +0000

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuvenProj::Application.config.secret_key_base = '8733d7b3edaddd8c644981428bfbecf2b25465c81231247514aa6d6a423aa8f974a1a2cf94f59fc89384db65e7110e1be6dc5df0209005f319f5483c05aa1e45'
